577472f6e1a45e12dee5c33d70306aeec271f2aa
Michał Dyło <dylomichal@gmail.com>
2025-01-25 14:01:48 +0100

    Add dialogue system

--- PolyJam 2025/Assets/_Assets/Scripts/Dialogue.cs
+++ PolyJam 2025/Assets/_Assets/Scripts/Dialogue.cs
@@ -1,29 +1,85 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
     [SerializeField] private Image _interactionTextImage;
-    [SerializeField] private Image[] _dialogueImages;
+    [SerializeField] private Image _dialogueImage;
+    [SerializeField] private Sprite[] _dialogueSprites;
+    private InputAction _interactAction;
+    private int _dialogueIndex = 0;
+    private bool _isInRange = false;
 
     private void Awake()
     {
         _interactionTextImage.enabled = false;
+        _interactAction = InputSystem.actions.FindAction("Interact");
+    }
+
+    private void Update()
+    {
+        if (_isInRange && _interactAction.WasPressedThisFrame())
+        {
+            _interactionTextImage.enabled = false;
+            _dialogueImage.enabled = true;
+            _dialogueImage.sprite = _dialogueSprites[_dialogueIndex];
+
+            if (_dialogueIndex < _dialogueSprites.Length - 1)
+            {
+                ++_dialogueIndex;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("PlayerTag"))
         {
+            _isInRange = true;
             _interactionTextImage.enabled = true;
         }
     }
+
+    // private void OnTriggerStay2D(Collider2D other)
+    // {
+    //     if (other.gameObject.CompareTag("PlayerTag"))
+    //     {
+    //         if (Keyboard.current.eKey.wasPressedThisFrame)
+    //         {
+    //             Debug.Log("E");
+    //             _interactionTextImage.enabled = false;
+    //             _dialogueImage.enabled = true;
+    //             _dialogueImage.sprite = _dialogueSprites[_dialogueIndex];
+
+    //             if (_dialogueIndex < _dialogueSprites.Length - 1)
+    //             {
+    //                 ++_dialogueIndex;
+    //             }
+    //         }
+    //     }
+    // }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("PlayerTag"))
         {
+            _isInRange = false;
             _interactionTextImage.enabled = false;
+            _dialogueImage.enabled = false;
+            _dialogueIndex = 0;
         }
     }
+
+    // private IEnumerator ShowDialogue()
+    // {
+    //     _dialogueImage.sprite = _dialogueSprites[_dialogueIndex];
+    //     yield return new WaitForSeconds(0.1f);
+
+    //     if (_dialogueIndex < _dialogueSprites.Length - 1)
+    //     {
+    //         ++_dialogueIndex;
+    //     }
+    // }
 }
