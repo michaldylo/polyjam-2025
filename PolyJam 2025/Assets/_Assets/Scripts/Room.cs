c9a7df90b496e196007c0e1c881f51f842e926e3
Michał Dyło <dylomichal@gmail.com>
2025-01-26 02:14:21 +0100

    Move room state randomization from Room.cs to RoomController.cs

--- PolyJam 2025/Assets/_Assets/Scripts/Room.cs
+++ PolyJam 2025/Assets/_Assets/Scripts/Room.cs
@@ -7,9 +7,10 @@ public class Room : MonoBehaviour
     [SerializeField] private Sprite[] _sprites = new Sprite[5];
     [SerializeField] private SpriteRenderer _spriteRenderer;
     public bool IsDirty { get; set; }
-    private Tool.ToolType _neededTool;
-    [SerializeField] private Sprite[] _toolIcons = new Sprite[5];
+    public Tool.ToolType NeededTool { get; set; }
+    // [SerializeField] private Sprite[] _toolIcons = new Sprite[5];
     [SerializeField] private SpriteRenderer _toolIconRenderer;
+    public SpriteRenderer ToolIconRenderer => _toolIconRenderer;
     private bool _playerIsInRange = false;
     private InputAction _interact1Action;
     private InputAction _interact2Action;
@@ -17,35 +18,35 @@ public class Room : MonoBehaviour
 
     private void Awake()
     {
-        IsDirty = Convert.ToBoolean(UnityEngine.Random.Range(0, 2));
+        _interact1Action = InputSystem.actions.FindAction("Interact1");
+        _interact2Action = InputSystem.actions.FindAction("Interact2");
+
+        // IsDirty = Convert.ToBoolean(UnityEngine.Random.Range(0, 2));
 
         if (IsDirty)
         {
-            _interact1Action = InputSystem.actions.FindAction("Interact1");
-            _interact2Action = InputSystem.actions.FindAction("Interact2");
+            // int toolId = UnityEngine.Random.Range(0, 5);
 
-            int toolId = UnityEngine.Random.Range(0, 5);
+            // switch (toolId)
+            // {
+            //     case 0:
+            //         NeededTool = Tool.ToolType.Sponge;
+            //         break;
+            //     case 1:
+            //         NeededTool = Tool.ToolType.Mop;
+            //         break;
+            //     case 2:
+            //         NeededTool = Tool.ToolType.VacuumCleaner;
+            //         break;
+            //     case 3:
+            //         NeededTool = Tool.ToolType.TrashCan;
+            //         break;
+            //     case 4:
+            //         NeededTool = Tool.ToolType.FeatherDuster;
+            //         break;
+            // }
 
-            switch (toolId)
-            {
-                case 0:
-                    _neededTool = Tool.ToolType.Sponge;
-                    break;
-                case 1:
-                    _neededTool = Tool.ToolType.Mop;
-                    break;
-                case 2:
-                    _neededTool = Tool.ToolType.VacuumCleaner;
-                    break;
-                case 3:
-                    _neededTool = Tool.ToolType.TrashCan;
-                    break;
-                case 4:
-                    _neededTool = Tool.ToolType.FeatherDuster;
-                    break;
-            }
-
-            _toolIconRenderer.sprite = _toolIcons[toolId];
+            // _toolIconRenderer.sprite = _toolIcons[toolId];
         }
     }
 
@@ -85,23 +86,23 @@ public class Room : MonoBehaviour
 
     private bool PlayerHasRightTool(ControllableCharacter.CharacterTypeEnum characterType)
     {
-        if (_neededTool == Tool.ToolType.Sponge && characterType == ControllableCharacter.CharacterTypeEnum.Son1)
+        if (NeededTool == Tool.ToolType.Sponge && characterType == ControllableCharacter.CharacterTypeEnum.Son1)
         {
             return true;
         }
-        else if (_neededTool == Tool.ToolType.Mop && characterType == ControllableCharacter.CharacterTypeEnum.Mom)
+        else if (NeededTool == Tool.ToolType.Mop && characterType == ControllableCharacter.CharacterTypeEnum.Mom)
         {
             return true;
         }
-        else if (_neededTool == Tool.ToolType.VacuumCleaner && characterType == ControllableCharacter.CharacterTypeEnum.Dad)
+        else if (NeededTool == Tool.ToolType.VacuumCleaner && characterType == ControllableCharacter.CharacterTypeEnum.Dad)
         {
             return true;
         }
-        else if (_neededTool == Tool.ToolType.TrashCan && characterType == ControllableCharacter.CharacterTypeEnum.Son2)
+        else if (NeededTool == Tool.ToolType.TrashCan && characterType == ControllableCharacter.CharacterTypeEnum.Son2)
         {
             return true;
         }
-        else if (_neededTool == Tool.ToolType.FeatherDuster && characterType == ControllableCharacter.CharacterTypeEnum.Daughter)
+        else if (NeededTool == Tool.ToolType.FeatherDuster && characterType == ControllableCharacter.CharacterTypeEnum.Daughter)
         {
             return true;
         }
--- PolyJam 2025/Assets/_Assets/Scripts/RoomController.cs
+++ PolyJam 2025/Assets/_Assets/Scripts/RoomController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class RoomController : MonoBehaviour
@@ -5,8 +6,50 @@ public class RoomController : MonoBehaviour
     [SerializeField] private int _numberOfRooms = 7;
     [SerializeField] private GameObject[] _playerOneRooms = new GameObject[7];
     [SerializeField] private GameObject[] _playerTwoRooms = new GameObject[7];
+    [SerializeField] private Sprite[] _toolIcons = new Sprite[5];
     private bool _playerOneWon = false;
     private bool _playerTwoWon = false;
+
+    private void Awake()
+    {
+        for (int i = 0; i < _playerOneRooms.Length; ++i)
+        {
+            bool isDirty = Convert.ToBoolean(UnityEngine.Random.Range(0, 2));
+            _playerOneRooms[i].GetComponent<Room>().IsDirty = isDirty;
+            _playerTwoRooms[i].GetComponent<Room>().IsDirty = isDirty;
+
+            if (isDirty)
+            {
+                int toolId = UnityEngine.Random.Range(0, 5);
+
+                switch (toolId)
+                {
+                    case 0:
+                        _playerOneRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.Sponge;
+                        _playerTwoRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.Sponge;
+                        break;
+                    case 1:
+                        _playerOneRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.Mop;
+                        _playerTwoRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.Mop;
+                        break;
+                    case 2:
+                        _playerOneRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.VacuumCleaner;
+                        _playerTwoRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.VacuumCleaner;
+                        break;
+                    case 3:
+                        _playerOneRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.TrashCan;
+                        _playerTwoRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.TrashCan;
+                        break;
+                    case 4:
+                        _playerOneRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.FeatherDuster;
+                        _playerTwoRooms[i].GetComponent<Room>().NeededTool = Tool.ToolType.FeatherDuster;
+                        break;
+                }
+
+                _playerOneRooms[i].GetComponent<Room>().ToolIconRenderer.sprite = _toolIcons[toolId];
+            }
+        }
+    }
 
     private void Update()
     {
@@ -19,7 +62,7 @@ public class RoomController : MonoBehaviour
             {
                 _playerOneWon = false;
             }
-            
+
             if (_playerTwoRooms[i].GetComponent<Room>().IsDirty)
             {
                 _playerTwoWon = false;
